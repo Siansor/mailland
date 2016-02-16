e0e4b411d2ccbd4827b88da22ff0a37b9211638f
Siansor <shapalov@gmail.com>
2016-02-16 17:05:33 +0200

    Update file

--- app.rb
+++ app.rb
@@ -11,22 +11,23 @@ post '/mail' do
   # @email =  params[:email]
 
     Pony.mail({
-      :from => 'shapalov@gmail.com',
-      :to => 'shapalov@gmail.com',
-      :subject => "Contact Message",
+      :from => 'info@doctravel.com',
+      :to => params[:email],
+      :cc => 'siansor@gmail.com',
+      :subject => "DocTravel презентация ",
+      :body => "Спасибо. Ваш линк для скачивания презентации http://doctravel.com/DocTravelRU.pdf",
       :via => :smtp,
       :via_options => {
         :address              => 'smtp.gmail.com',
         :port                 => '587',
         :enable_starttls_auto => true,
-        :user_name            => 'sasha@gmail.com',
-        :password             => '111111',
+        :user_name            => 'shapalov@gmail.com',
+        :password             => '',
         :authentication       => :plain, # :plain, :login, :cram_md5, no auth by default
         :domain               => "localhost.localdomain" # the HELO domain provided by the client to the server
       }
     })
-    redirect '/stream'
-
+    redirect '/thanks'
 end
 
 
@@ -36,7 +37,10 @@ class Stream
   end
 end
 
-get '/stream' do
-  erb :stream
-  Stream.new
+get '/thanks' do
+  erb :thanks
+end
+
+get '/DocTravelRU.pdf' do
+  attachment 'DocTravelRU.pdf'
 end
