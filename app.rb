8f3053704ad44b572fa2184f80cfe138c74e14d7
Sasha <Siansor@gmail.com>
2016-02-17 10:27:08 -0500

    Update sinatra

--- app.rb
+++ app.rb
@@ -18,13 +18,12 @@ post '/mail' do
       :body => "Contact link <a href='http://doctravel.com/DocTravelRU.pdf'> Link </a> ",
       :via => :smtp,
       :via_options => {
-        :address              => 'smtp.gmail.com',
-        :port                 => '587',
-        :enable_starttls_auto => true,
-        :user_name            => 'shapalov@gmail.com',
-        :password             => '',
-        :authentication       => :plain, # :plain, :login, :cram_md5, no auth by default
-        :domain               => "localhost.localdomain" # the HELO domain provided by the client to the server
+        address: "smtp.mandrillapp.com",
+        port: 587,
+        domain: "doctravel.com",
+        authentication: "plain",
+        user_name: "info@doctravel.com",
+        password: "6SSKZqhx1KBoEAdvHhjSug"
       }
     })
     redirect '/thanks'
--- config.ru
+++ config.ru
@@ -2,4 +2,10 @@ require 'rubygems'
 require 'sinatra'
 require File.expand_path '../app.rb', __FILE__
 
-run Sinatra::Application
+enable :logging
+#set :server, 'Thin'  
+set :environment, :production
+set :bind, '178.62.227.195'
+#set :port, 4567
+
+run app.rb
