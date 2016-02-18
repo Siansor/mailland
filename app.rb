232debf4e2c7a0628cc33de788f1f4aa97f4eb10
Siansor <shapalov@gmail.com>
2016-02-18 15:16:39 +0200

    Add english page

--- app.rb
+++ app.rb
@@ -3,34 +3,55 @@ require 'sinatra/reloader'
 require 'pony'
 
 get '/' do
-  erb :index
+  erb :indexeng
 end
 
-post '/mail' do
-  # @name =  params[:name]
-  # @email =  params[:email]
-
-    Pony.mail({
-      :from => 'info@doctravel.com',
-      :to => params[:email],
-      :bcc => 'shapalov@gmail.com',
-      :bcc => 'info@doctravel.com',
-      :bcc => 'rolenchyn@doctravel.com',
-      :subject => "Doctravel - ключ ко всей зарубежной медицине",
-      :body => "Спасибо за ваш интерес к нашему проекту! Вы можете скачать презентацию проекта по этому линку: https://s3.amazonaws.com/uploads.hipchat.com/471390/3011413/D0OIllENbV6SOcj/DocTravel%20ustartup.pdf Хорошего дня!",
-      :via => :smtp,
-      :via_options => {
-        address: "smtp.mandrillapp.com",
-        port: 587,
-        domain: "doctravel.com",
-        authentication: "plain",
-        user_name: "info@doctravel.com",
-        password: "6SSKZqhx1KBoEAdvHhjSug"
-      }
-    })
-    redirect '/thanks'
+get '/indexru' do
+  erb :indexru
 end
 
+
+post '/thankseng' do
+  Pony.mail({
+    :from => 'info@doctravel.com',
+    :to => params[:email],
+    :bcc => 'info@doctravel.com',
+    :bcc => 'rolenchyn@doctravel.com',
+    :subject => "Doctravel - ключ ко всей зарубежной медицине",
+    :body => "Спасибо за ваш интерес к нашему проекту! Вы можете скачать презентацию проекта по этому линку: https://s3.amazonaws.com/uploads.hipchat.com/471390/3011413/D0OIllENbV6SOcj/DocTravel%20ustartup.pdf Хорошего дня!",
+    :via => :smtp,
+    :via_options => {
+      address: "smtp.mandrillapp.com",
+      port: 587,
+      domain: "doctravel.com",
+      authentication: "plain",
+      user_name: "info@doctravel.com",
+      password: ""
+    }
+  })
+  redirect '/thankseng'
+end
+
+post '/thanksru' do
+  Pony.mail({
+    :from => 'info@doctravel.com',
+    :to => params[:email],
+    :bcc => 'info@doctravel.com',
+    :bcc => 'rolenchyn@doctravel.com',
+    :subject => "DocTravel - your personal access key to all foreign medicine",
+    :body => "Thank you for your interest to our project! You can download the presentation of our project by this link: https://s3.amazonaws.com/uploads.hipchat.com/471390/3011413/D0OIllENbV6SOcj/DocTravel%20ustartup.pdf Have a good day!",
+    :via => :smtp,
+    :via_options => {
+      address: "smtp.mandrillapp.com",
+      port: 587,
+      domain: "doctravel.com",
+      authentication: "plain",
+      user_name: "info@doctravel.com",
+      password: ""
+    }
+  })
+  redirect '/thanksru'
+end
 
 class Stream
   def each
@@ -38,9 +59,14 @@ class Stream
   end
 end
 
-get '/thanks' do
-  erb :thanks
+get '/thankseng' do
+  erb :thankseng
 end
+
+get '/thanksru' do
+  erb :thanksru
+end
+
 
 get '/DocTravelRU.pdf' do
   attachment 'DocTravelRU.pdf'
